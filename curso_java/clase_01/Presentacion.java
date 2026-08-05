b113b9ceb1466b32e7e8402f417af841b4b28d59
Juan Daniel Sierra <juandanieljdsda10@gmail.com>
2026-08-04 22:40:15 -0500

    feat: complete class 01 exercises

---
+++ curso_java/clase_01/Presentacion.java
@@ -0,0 +1,10 @@
+public class Presentacion {
+  public static void main(String[] args){
+    System.out.println("=========================");
+    System.out.println("Hola, soy Juan.");
+    System.out.println("Estoy aprendiendo Java.");
+    System.out.println("Este es mi primer programa.");
+    System.out.println("Mi objetivo es ser Backend Developer.");
+    System.out.println("=========================");
+  }
+}
